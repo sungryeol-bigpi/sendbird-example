0b4a4535b62755711f40f8b1c087c0867982335a
rabelais88 <sungryeolp@gmail.com>
2019-03-05 17:16:28 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import sb from './sendbird';
+import SB from './sendbird';
+
+const sb = new SB('833BB791-D31C-4277-A840-70991E0BBD17');
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sb: new sb('833BB791-D31C-4277-A840-70991E0BBD17'),
       user: null,
       channel: null,
       channels: [],
@@ -18,9 +19,9 @@ class App extends Component {
   
   async connect() {
     console.log('connection req');
-    const user = await this.state.sb.connect();
-    const channel = await this.state.sb.connectChannel('sendbird_open_channel_48868_c7b51ce319f8414d1c6ce6ad8d016c004eaa97e9');
-    const channels = await this.state.sb.getChannels();
+    const user = await sb.connect();
+    const channel = await sb.connectChannel('sendbird_open_channel_48868_c7b51ce319f8414d1c6ce6ad8d016c004eaa97e9');
+    const channels = await sb.getChannels();
     this.setState({
       user,
       channel,
@@ -32,6 +33,7 @@ class App extends Component {
     const itmChannel = (channel, idx) => <li key={idx}>{channel.name}</li>
     return (<div>
         <p>connected</p>
+        <h2>current channels available</h2>
         <ul>
           {!this.state.channels ? null: this.state.channels.map(itmChannel)}
         </ul>
